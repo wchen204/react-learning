58a1cd9b8cf7b5465e6f71c17f2dfabae31c6fc0
Wei Chen <veradec@outlook.com>
2025-03-03 20:05:46 -0500

    build-in validation methods

--- projects/form-user-input/src/components/Signup.jsx
+++ projects/form-user-input/src/components/Signup.jsx
@@ -17,6 +17,8 @@ export default function Signup() {
         event.target.reset(); // reset the form.
     }
 
+    // build in form validation
+    // https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/Form_validation
     return (
         <form onSubmit={handleSubmit}>
             <h2>Welcome on board!</h2>
@@ -24,13 +26,17 @@ export default function Signup() {
 
             <div className="control">
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" />
+                <input id="email" type="email" name="email" required/>
             </div>
 
             <div className="control-row">
                 <div className="control">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password" />
+                    <input id="password"
+                           type="password"
+                           name="password"
+                           minLength="6"
+                           required/>
                 </div>
 
                 <div className="control">
@@ -39,27 +45,28 @@ export default function Signup() {
                         id="confirm-password"
                         type="password"
                         name="confirm-password"
+                        required
                     />
                 </div>
             </div>
 
-            <hr />
+            <hr/>
 
             <div className="control-row">
                 <div className="control">
                     <label htmlFor="first-name">First Name</label>
-                    <input type="text" id="first-name" name="first-name" />
+                    <input type="text" id="first-name" name="first-name" required/>
                 </div>
 
                 <div className="control">
                     <label htmlFor="last-name">Last Name</label>
-                    <input type="text" id="last-name" name="last-name" />
+                    <input type="text" id="last-name" name="last-name" required/>
                 </div>
             </div>
 
             <div className="control">
                 <label htmlFor="phone">What best describes your role?</label>
-                <select id="role" name="role">
+                <select id="role" name="role" required>
                     <option value="student">Student</option>
                     <option value="teacher">Teacher</option>
                     <option value="employee">Employee</option>
@@ -91,14 +98,14 @@ export default function Signup() {
                 </div>
 
                 <div className="control">
-                    <input type="checkbox" id="other" name="acquisition" value="other" />
+                    <input type="checkbox" id="other" name="acquisition" value="other"/>
                     <label htmlFor="other">Other</label>
                 </div>
             </fieldset>
 
             <div className="control">
                 <label htmlFor="terms-and-conditions">
-                    <input type="checkbox" id="terms-and-conditions" name="terms" />I
+                    <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
                     agree to the terms and conditions
                 </label>
             </div>
